d70d3f2cc98c625f76a8b57115a4ab65c9491fc7
PlamenMichev <plamenmichev15@gmail.com>
2021-12-18 11:59:54 +0100

    fix navigation

--- common.js
+++ common.js
@@ -1,15 +1,18 @@
 function toggleHamburger(element) {
     const mobileNavElements = document.getElementsByClassName('nav-links')[0];
     const mobileLogoElement = document.getElementById('nav-logo-mobile');
+    const nav = document.getElementsByTagName('nav')[0];
     const isActive = mobileNavElements.classList.contains('active')
 
     if (isActive) {
         mobileNavElements.classList.remove('active');
         mobileLogoElement.classList.remove('active');
+        nav.classList.remove('active');
         // document.body.style.overflowY = 'hidden'
     } else {
         mobileNavElements.classList.add('active');
         mobileLogoElement.classList.add('active');
+        nav.classList.add('active');
         // document.body.style.overflowY = 'auto'
     }
 
